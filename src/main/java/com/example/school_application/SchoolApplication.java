b4777d3fd53402ffd59886ab6453255ce1d6383e
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-02-21 18:10:39 +0530

    add formatter as well add below config in vscode settings.json ``` "[java]": {     "editor.defaultFormatter": "josevseb.google-java-format-for-vs-code",     "editor.codeActionsOnSave": {       "source.organizeImports": "explicit"     }   },   ```

--- src/main/java/com/example/school_application/SchoolApplication.java
+++ src/main/java/com/example/school_application/SchoolApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolApplication.class, args);
+  }
 }
